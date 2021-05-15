700f779215e55af8f6994616fa8d866b0612f3f6
Rakibur Rahman <rakibur5454@gmail.com>
2021-05-16 02:27:02 +0600

    crud done

--- controllers/posts.js
+++ controllers/posts.js
@@ -47,3 +47,13 @@ export const deletePost = async (req, res) => {
 
     res.json({ message: 'Post deleted Successfully' })
 }
+
+export const likePost = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post found')
+    const post = await PostMessage.findById(id)
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true })
+
+    res.json(updatedPost)
+}
