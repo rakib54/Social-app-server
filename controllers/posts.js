b13ebb41bd9bc8d991f5bce17ed495264820e710
Rakibur Rahman <rakibur5454@gmail.com>
2021-05-16 00:11:52 +0600

    update and delete done

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import PostMessage from '../models/postMessage.js'
 
 export const getPosts = async (req, res) => {
@@ -13,7 +14,7 @@ export const getPosts = async (req, res) => {
 }
 export const createPost = async (req, res) => {
     const post = req.body
-    const newPost =  new PostMessage(post)
+    const newPost = new PostMessage(post)
 
     try {
         await newPost.save()
@@ -21,7 +22,28 @@ export const createPost = async (req, res) => {
         res.status(201).json(newPost)
 
     } catch (error) {
-        
+
         res.status(409).json({ error: error.message })
     }
+}
+
+export const updatePost = async (req, res) => {
+    const { id: _id } = req.params;
+    const post = req.body
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post found')
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+
+    res.json(updatedPost)
+}
+
+export const deletePost = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post found')
+
+    await PostMessage.findByIdAndRemove(id)
+
+    res.json({ message: 'Post deleted Successfully' })
 }
