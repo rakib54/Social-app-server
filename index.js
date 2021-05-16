c4e428bdc2a58fd4a08a75e5f7f9cffcfe117751
Rakibur Rahman <rakibur5454@gmail.com>
2021-05-16 14:54:36 +0600

    env edited

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.use('/posts',postRoutes);
 
 const PORT = process.env.PORT || 4000
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const CONNECTION_URL= `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c4bol.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT,()=>console.log(`server running at ${PORT}`)))
     .catch(err =>console.log(err.message))
 
